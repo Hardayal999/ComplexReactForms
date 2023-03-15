5b29017520fed2692a57e00953e74b06cb9880b1
Hardayal Punshi <arorap876@gmail.com>
2023-03-15 21:43:34 +1000

    adding useState and useRef examples for the forms

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,29 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const userInput = useRef("");
+  // const [enteredName, setEnteredName] = useState("");
+
+  // const nameInputChangeHandler = (event) => {
+  //   setEnteredName(event.target.value);
+  // };
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    const enteredValue = userInput.current.value;
+    console.log(enteredValue);
+    // console.log(enteredName);
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={userInput}
+          type="text"
+          id="name"
+          // value={enteredName}
+          // onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
